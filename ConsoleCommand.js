5f300f7c0995c57c4c7d3e2a3af08d280a504b19
BluAxolotl <plush.gamer.456@gmail.com>
2021-09-07 20:34:42 -0400

    Finished (required) checklist
    + Dynamic IP addresses supported
    + 'console_server_config.json' added and implemented

--- ConsoleCommand.js
+++ ConsoleCommand.js
@@ -2,8 +2,22 @@
 
 var chalk = require('chalk');
 const internalIp = require('internal-ip')
-var config = require('../../console_server_config.json') // Goes from 'console-to-server' to 'node_modules' to the package base folder
-var enabled = process.env.CONSERV
+var fs = require('fs')
+var inited = false
+var config = null
+var enabled = null
+if (process.env.DEV == null) {
+	if (fs.existsSync('../../console_server_config.json')) {
+		config = require('../../console_server_config.json') // Goes from 'console-to-server' to 'node_modules' to the package base folder
+	}
+	enabled = process.env.CONSERV
+	if (config.require_enable == false) {
+		enabled = true
+	}
+} else {
+	config = require('./console_server_config.json')
+	enabled = true
+}
 
 var Commands = {
 	array: [],
@@ -38,7 +52,6 @@ const { Server } = require("socket.io");
 const http = require('http');
 const console_server = http.createServer(console_app);
 const readline = require('readline')
-var fs = require('fs')
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -71,16 +84,21 @@ if (enabled) {
 	rl.on('line', (input) => {
 	  let run = false
 	  let args = input.split(" ")
-	  if (config.console.prefix) {
+	  if (config.console && config.console.prefix) {
 		let prefix = args.shift()
-		run = (prefix == config.console.prefix)
+		run = (config.console && prefix == config.console.prefix)
 	  } else {
 		run = true
 	  }
 	  let command_name = args.shift()
 	  if (run) {
 		  try {
-			Commands.dict[command_name].exec(args)
+		  	let command = Commands.dict[command_name]
+		  	if (command) {
+		  		command.exec(args)
+		  	} else {
+		  		print_debug(`Command '${command_name}' does not exist`)
+		  	}
 		  } catch (err) {
 			print_debug(String(err))
 		  }
@@ -90,45 +108,62 @@ if (enabled) {
 
 // Default Commands ⬇⬇⬇
 
-new ConsoleCommand("help", "Lists all commands and their functions", ["none"], function() {
-	let arr = []
-	Commands.array.forEach(i => {
-		arr.push(`${i.name}: ${i.desc} (${i.args.join(", ")})`)
+if (config.console == null || config.console.default_commands == true) {
+	new ConsoleCommand("help", "Lists all commands and their functions", ["none"], function() {
+		let arr = []
+		Commands.array.forEach(i => {
+			arr.push(`${i.name}: ${i.desc} (${i.args.join(", ")})`)
+		})
+		print_debug(arr.join("\n"))
 	})
-	print_debug(arr.join("\n"))
-})
 
-new ConsoleCommand("ping", "Responds with pong!", ["none"], function() {
-	print_debug("pong")
-})
+	new ConsoleCommand("ping", "Responds with pong!", ["none"], function() {
+		print_debug("pong")
+	})
 
-new ConsoleCommand("testargs", "Test command for arguments", ["arg1", "arg2"], function(args) {
-	print_debug(`The arguments were ${args[0]} & ${args[1]}`)
-})
+	new ConsoleCommand("testargs", "Test command for arguments", ["arg1", "arg2"], function(args) {
+		print_debug(`The arguments were ${args[0]} & ${args[1]}`)
+	})
 
-new ConsoleCommand("send", "Sends to web server", ["stuff..."], function(args) {
-	console_io.emit("print", (args.join(" ")))
-})
+	new ConsoleCommand("send", "Sends to web server", ["stuff..."], function(args) {
+		console_io.emit("print", (args.join(" ")))
+	})
 
-new ConsoleCommand("crash", "Intentionally crashes nodejs", ["none"], function(args) {
-	nonexistant()
-})
+	new ConsoleCommand("crash", "Intentionally crashes nodejs", ["none"], function(args) {
+		nonexistant()
+	})
+}
 
 // Server Redirect and Listen ⬇⬇⬇
 
-console_app.get('/console', (req, res) => {
-  res.sendFile(__dirname + '/console.html'); // Fix icon/logo/favicon thing
+var server_path = (config.server && config.server.path ? config.server.path : "/console")
+
+console_app.get(server_path, (req, res) => {
+  res.sendFile(__dirname + '/console.html') // Fix icon/logo/favicon thing
 })
 
-var server_host = (config.server.host ? config.server.host : internalIp.v4.sync())
-var server_port = (config.server.port ? config.server.port : 8000)
+var server_host = (config.server && config.server.host ? config.server.host : internalIp.v4.sync())
+var server_port = (config.server && config.server.port ? config.server.port : 8000)
+
+console_io.on('connection', socket => {
+	if (config.html != null && config.html.styles != null) {
+		socket.emit("html_init", JSON.stringify(config.html.styles))
+	}
+})
 
 if (enabled) {
 	console_server.listen({
 		host: server_host, // Make this dynamic to get current machine's ipv4 address and also interact with config json
 		port: server_port // Make this interact with config json
 	}, () => {
-		print(`listening on *:${server_port}`);
+		inited = true
+		if (config.print_link == true) {
+			print(`ConsoleServer @http://${server_host}:${server_port}${server_path}`)
+		}
+
+		if (config.html != null && config.html.styles != null) {
+			console_io.emit("html_init", JSON.stringify(config.html.styles))
+		}
 	})
 }
 
@@ -143,17 +178,17 @@ module.exports = {
 // My personal to-do list ⬇⬇⬇
 
 // ## List of stuff to add
-// [-] Get machine's IvP4 address to set default Host
-// [-] Add in 'console_server_config.json' functionality
-// [-] Host changes to 'console_server_config.json'
-// [-] Port changes to 'console_server_config.json'
-// [-] Prefix support (enabling and setting) in 'console_server_config.json'
-// [ ] Set custom HTML path in 'console_server_config.json'
-// [ ] Adjusting simple HTML parameters (styling, font, text size, show timestamps?) in 'console_server_config.json'
-// [ ] Fix icon/logo/favicon thing on html
+// [x] Get machine's IvP4 address to set default Host
+// [x] Add in 'console_server_config.json' functionality
+// [x] Host changes to 'console_server_config.json'
+// [x] Port changes to 'console_server_config.json'
+// [x] Prefix support (enabling and setting) in 'console_server_config.json'
+// [x] Set custom HTML path in 'console_server_config.json'
+// [x] Adjusting simple HTML parameters (styling, font, text size, show timestamps?) in 'console_server_config.json'
+// [x] Fix icon/logo/favicon thing on html
 // [ ] Update README to show it working on mobile phone browser
-// [ ] Environmental Variable that toggles the module (off by default for deploying purposes)
-// [ ] Print ip of ConsoleServer when initializing (toggable in 'console_server_config.json')
-// [ ] Toggle default commands in 'console_server_config.json'
-// ( ) Public Console feature: Allows people to view console server after being deployed (Oh?)
+// [x] Environmental Variable that toggles the module (off by default for deploying purposes)
+// [x] Print ip of ConsoleServer when initializing (toggable in 'console_server_config.json')
+// [x] Toggle default commands in 'console_server_config.json'
+// (x) Public Console feature: Allows people to view console server after being deployed (Oh?)
 // ( ) Look into directly streaming all console output
